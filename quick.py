1fb586966bfb7e1534a292f65065fcd710cde9bd
qbuat <quentin.buat@cern.ch>
2017-11-07 13:57:08 +0100

    automatize for all theory nps

--- quick.py
+++ quick.py
@@ -8,6 +8,20 @@ import ROOT
 set_style('ATLAS', shape='rect')
 ROOT.gROOT.SetBatch(True)
 
+
+
+def make_plot(measurements, categories):
+    for meas, cat in zip(measurements, categories):
+        ztt = meas.get_sample('Ztt')
+        systs = ztt.syst_dict(cat.cats, meas.rfile)
+        keys = filter(lambda s: 'theory' in s, systs.keys())
+        print meas.channel
+        for k in keys:
+            print '\t', k
+            c = raw_np_plot(meas.rfile, k, cat, ztt)
+            c.SaveAs('plots/{}_{}_{}.pdf'.format(k, meas.channel, cat.name.replace(' ', '_')))
+
+
 # hh_file = root_open('/Users/quentin/cernbox/workspaces/htt_sept28/hhAll_merged_WSinput_WithTheoryEnv_WithModJERVar_rebinned.root')
 # hh_file = root_open('/Users/quentin/cernbox/workspaces/htt_fine_binning/hhAll_merged_WSinput_WithTheoryEnv_WithModJERVar.root')
 # lh_file = root_open('/Users/quentin/cernbox/workspaces/htt_fine_binning/lephad_fs46_MMC_Fine_BL_WithModJERVar_WithTheoryEnv.root')
@@ -22,25 +36,24 @@ ll_m =  measurement(ll_file, 'leplep')
 
 measurements = [hh_m, lh_m, ll_m]
 
-np_name = 'theory_ztt_qsf'
-for meas, cat in zip(measurements, CATEGORIES_BOOST_MERGED):
-    ztt = meas.get_sample('Ztt')
-    systs = ztt.systematics(cat.cats, meas.rfile)
-    systs_th = filter(lambda s: 'theory' in s, systs[0])
-    print cat.name
-    print systs_th
-    print
-    c = raw_np_plot(meas.rfile, np_name, cat, ztt)
-    c.SaveAs('{}_{}_{}.pdf'.format(np_name, meas.channel, cat.name.replace(' ', '_')))
+# np_name = 'theory_ztt_qsf'
 
-for meas, cat in zip(measurements, CATEGORIES_VBF_MERGED):
-    ztt = meas.get_sample('Ztt')
-    systs = ztt.systematics(cat.cats, meas.rfile)
-    systs_th = filter(lambda s: 'theory' in s, systs[0])
-    print cat.name
-    print systs_th
-    print
-    c = raw_np_plot(meas.rfile, np_name, cat, ztt)
-    c.SaveAs('{}_{}_{}.pdf'.format(np_name, meas.channel, cat.name.replace(' ', '_')))
+make_plot(measurements, CATEGORIES_BOOST_MERGED)
+make_plot(measurements, CATEGORIES_VBF_MERGED)
+
+# for meas, cat in zip(measurements, CATEGORIES_BOOST_MERGED):
+#     ztt = meas.get_sample('Ztt')
+#     systs = ztt.syst_dict(cat.cats, meas.rfile)
+#     keys = filter(lambda s: 'theory' in s, systs.keys())
+#     print meas.channel
+#     for k in keys:
+#         print '\t', k
+#         c = raw_np_plot(meas.rfile, k, cat, ztt)
+#         c.SaveAs('plots/{}_{}_{}.pdf'.format(k, meas.channel, cat.name.replace(' ', '_')))
+
+# for meas, cat in zip(measurements, CATEGORIES_VBF_MERGED):
+#     ztt = meas.get_sample('Ztt')
+#     c = raw_np_plot(meas.rfile, np_name, cat, ztt)
+#     c.SaveAs('{}_{}_{}.pdf'.format(np_name, meas.channel, cat.name.replace(' ', '_')))
 
 # print [c.name for c in CATEGORIES_VBF_MERGED]
